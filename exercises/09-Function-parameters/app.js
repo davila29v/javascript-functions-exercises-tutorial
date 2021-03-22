d811c51dd79c5861fd34ed13b537d50e16add052
davila29v <davila29v@hotmail.com>
2021-03-22 19:14:57 +0000

    avances

--- exercises/09-Function-parameters/app.js
+++ exercises/09-Function-parameters/app.js
@@ -1,6 +1,13 @@
 // Your code goes here:
+function renderPerson (name,birth,color,age,sex) {
+//let name = "jorge"
+//let birth = "today"
+//let color = "red"
+//let age = "5"
+//let sex = "female"
+return String (name+" "+"is"+" "+"a"+" "+ age +" "+ "years"+" "+ "old" +" "+ sex +" "+ "born"+" "+ "in" +" "+ birth +" "+ "with" +" "+ color +" "+ "eyes");
 
-
+}
 
   ///*** Do not edit below this line ****/
   console.log(renderPerson('Bob', '05/22/1983', 'green', 23, 'male'));
